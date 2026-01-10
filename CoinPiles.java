6bcffd744dbe03a94d46300503a161721e8bdc3f
Tejas Bansal <tejasbansal6065@gmail.com>
2026-01-10 15:26:43 +0530

    coin pile

--- CoinPiles.java
+++ CoinPiles.java
@@ -1,11 +1,25 @@
 // https://cses.fi/problemset/task/1754
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class CoinPiles {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb  = new StringBuilder();
 
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while(t-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+
+            sb.append( (a+b)%3 == 0 && Math.min(a, b) * 2 >= Math.max(a, b) ? "YES" : "NO").append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
